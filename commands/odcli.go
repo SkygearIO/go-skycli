2f465add6f70247cba3ebff0c2e1fa43b70f620d
Kwok-kuen Cheung <cheungpat@oursky.com>
2015-09-16 15:59:52 +0800

    Specify endpoint, api_key and access_token. #3

--- commands/odcli.go
+++ commands/odcli.go
@@ -1,17 +1,37 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/oursky/ourd-cli/container"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 var OurdCliCmd = &cobra.Command{
 	Use:   "odcli",
 	Short: "Command line interface to Ourd",
 }
 
+var ourdAPIKey string
+var ourdEndpoint string
+var ourdAccessToken string
+
 func init() {
+	OurdCliCmd.PersistentFlags().StringVar(&ourdAPIKey, "api_key", "", "API Key")
+	OurdCliCmd.PersistentFlags().StringVar(&ourdEndpoint, "endpoint", "", "Endpoint address")
+	OurdCliCmd.PersistentFlags().StringVar(&ourdAccessToken, "access_token", "", "Access token")
+
+	viper.BindPFlag("access_token", OurdCliCmd.PersistentFlags().Lookup("access_token"))
+	viper.BindPFlag("endpoint", OurdCliCmd.PersistentFlags().Lookup("endpoint"))
+	viper.BindPFlag("api_key", OurdCliCmd.PersistentFlags().Lookup("api_key"))
 
 }
 
 func Execute() {
+	viper.SetEnvPrefix("odcli")
+	viper.AutomaticEnv()
+
+	viper.SetDefault("endpoint", "http://localhost:3000")
+
 	AddCommands()
 	OurdCliCmd.Execute()
 }
